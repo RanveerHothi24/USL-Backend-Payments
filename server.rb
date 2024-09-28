be26484163c787a5600795488b3a356af8b1ced2
RanveerHothi24 <158070506+RanveerHothi24@users.noreply.github.com>
2024-09-28 16:40:00 +0100

    Direct success page redirection temporarily

--- server.rb
+++ server.rb
@@ -45,12 +45,12 @@ post '/create-checkout-session' do
 
     session = Stripe::Checkout::Session.create({
       line_items: [{
-        price: 'price_1Q3Kq0HThqRnB4qJGhU8z1xr', # Replace with actual price ID
+        price: 'price_1Q3Kq0HThqRnB4qJGhU8z1xr', 
         quantity: 1,
       }],
       mode: 'payment',
-      success_url: "#{YOUR_DOMAIN}/SignUp.html?payment_status=success",
-      cancel_url: "#{YOUR_DOMAIN}/SignUp.html",
+      success_url: "#{YOUR_DOMAIN}/success.html",
+      cancel_url: "#{YOUR_DOMAIN}/Sign.html",
     })
 
     # Log success
